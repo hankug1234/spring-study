f77e0ae8cf6d7366932f671d9946153ae1fe93c1
hankug1234 <hankug12@gmail.com>
2022-10-11 11:02:31 +0900

    -

--- src/main/java/tacos/web/DesignTacoController.java
+++ src/main/java/tacos/web/DesignTacoController.java
@@ -1,5 +1,6 @@
 package tacos.web;
 
+import java.security.Principal;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -21,8 +22,10 @@ import tacos.Ingredient;
 import tacos.Ingredient.Type;
 import tacos.Order;
 import tacos.Taco;
+import tacos.User;
 import tacos.data.IngredientRepository;
 import tacos.data.TacoRepository;
+import tacos.data.UserRepository;
 
 
 @Slf4j
@@ -32,15 +35,18 @@ import tacos.data.TacoRepository;
 public class DesignTacoController {
 	private final IngredientRepository ingredientRepo;
 	private TacoRepository tacoRepo;
+	private UserRepository userRepo;
 	
 	@Autowired
-	public DesignTacoController(IngredientRepository ingredientRepo, TacoRepository tacoRepo) {
+	public DesignTacoController(IngredientRepository ingredientRepo, TacoRepository tacoRepo,
+			UserRepository userRepo) {
 		this.ingredientRepo = ingredientRepo;
 		this.tacoRepo = tacoRepo;
+		this.userRepo = userRepo;
 	}
 
 	@GetMapping
-	public String showDesignForm(Model model) {
+	public String showDesignForm(Model model,Principal principal) {
 	
 		List<Ingredient> ingredients = new ArrayList<>();
 		ingredientRepo.findAll().forEach(i -> ingredients.add(i));
@@ -50,6 +56,10 @@ public class DesignTacoController {
 			model.addAttribute(type.toString().toLowerCase(),
 			filterBytype(ingredients,type));
 		}
+		
+		String username = principal.getName();
+		User user = userRepo.findByUsername(username);
+		model.addAttribute("user",user);
 		
 		return "design";
 	}
